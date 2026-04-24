5eaff832698e5727d69fb919d5fad7f3be9668f0
“Xb2555” <2012753288@qq.com>
2026-04-24 19:44:13 +0800

    feat: Build the parent module skeleton

---
+++ im-collab-ai-assistant-server/src/main/java/com/lark/imcollab/ImCollabAiAssistantServerApplication.java
@@ -0,0 +1,13 @@
+package com.lark.imcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImCollabAiAssistantServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImCollabAiAssistantServerApplication.class, args);
+    }
+
+}
---
+++ im-collab-ai-assistant-server/src/test/java/com/lark/imcollab/ImCollabAiAssistantServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lark.imcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImCollabAiAssistantServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
