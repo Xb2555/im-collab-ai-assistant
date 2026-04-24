91c6d24ccbece5baf4423d9383023d5bfe1d7e16
“Xb2555” <2012753288@qq.com>
2026-04-24 19:56:32 +0800

    feat: Build the common module skeleton

--- im-collab-ai-assistant-server/src/main/java/com/lark/imcollab/parent/ImCollabAiAssistantServerApplication.java
+++ im-collab-ai-assistant-server/src/main/java/com/lark/imcollab/parent/ImCollabAiAssistantServerApplication.java
@@ -1,4 +1,4 @@
-package com.lark.imcollab;
+package com.lark.imcollab.parent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
